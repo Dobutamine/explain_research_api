84c00b32ffc48a16e2226cb60d2060871085317b
dobutamine <tim.antonius@outlook.com>
2022-07-16 23:36:32 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 import { Model } from "./explain/model.js";
+import { WebSocketServer } from "ws";
+import express from "express";
+
+const app = express();
+
+// set up a headless websocket server
+const wsServer = new WebSocketServer({ noServer: true });
+
+// spin up the server and handle an upgrade request to upgrade the connection to a websocket connection
+const server = app.listen(3000);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
+});
 
 // instantiate a new model
 const model = new Model();
+
+model.events.on("ready", (id) => console.log(id));
