d9f437f01226ba6d4ae4d66e63a86e23062ed122
dobutamine <tim.antonius@outlook.com>
2022-07-17 00:06:39 +0200

    add websocket functionality

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 import { Model } from "./explain/model.js";
+import { createServer } from "https";
 import { WebSocketServer } from "ws";
+import { readFileSync } from "fs";
 import express from "express";
 
+// create an express application
 const app = express();
 
-// set up a headless websocket server
-const wsServer = new WebSocketServer({ noServer: true });
+// spin up the express server with https
+const server = createServer(
+  {
+    key: readFileSync("./api/key.pem"),
+    cert: readFileSync("./api/cert.pem"),
+  },
+  app
+).listen(3000);
 
-// spin up the server and handle an upgrade request to upgrade the connection to a websocket connection
-const server = app.listen(3000);
+// handle the upgrade request for a websocket connection
 server.on("upgrade", (request, socket, head) => {
   wsServer.handleUpgrade(request, socket, head, (socket) => {
     wsServer.emit("connection", socket, request);
   });
 });
 
+// set up a headless websocket server
+const wsServer = new WebSocketServer({ noServer: true });
+
+// handle events on the websocket server
+wsServer.on("connection", (client) => {
+  client.on("message", (data) => {
+    console.log(JSON.parse(data));
+  });
+});
+
 // instantiate a new model
 const model = new Model();
-
 model.events.on("ready", (id) => console.log(id));
